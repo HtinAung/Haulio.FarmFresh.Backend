42e6bfcaba2d2f9dfe7d98700c65ba53fb037f87
Mirza Ghulam Rasyid <mirza.rasyid@kobkiat-it.com>
2022-01-21 02:29:43 +0700

    Modified nullable store-id on AppUser

--- SharedLibraries/FarmFresh.Backend.Storages/Migrations/Common/20220120192832_ModifiedNullableColumnOnAppUser.Designer.cs
+++ SharedLibraries/FarmFresh.Backend.Storages/Migrations/Common/20220120192832_ModifiedNullableColumnOnAppUser.Designer.cs
@@ -4,14 +4,16 @@ using FarmFresh.Backend.Storages.SQLServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FarmFresh.Backend.Storages.Migrations.Common
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220120192832_ModifiedNullableColumnOnAppUser")]
+    partial class ModifiedNullableColumnOnAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
